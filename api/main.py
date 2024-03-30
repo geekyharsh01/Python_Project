529c4ee709c0c42eb1802db01c177eda3597e91e
Harsh Kumar <95271790+geekyharsh01@users.noreply.github.com>
2024-03-30 13:29:25 +0530

    Updated main and models files, Solved  input image error

--- api/main.py
+++ api/main.py
@@ -1,28 +1,30 @@
 # app/main.py
 from fastapi import FastAPI, File, UploadFile
+from PIL import Image
 from models import predict_cellphone
+from io import BytesIO
+import numpy as np
 
 app = FastAPI()
 
-list_of_image= []
+list_of_image = []
 
-
+async def read_imagefile(file) -> Image.Image:
+    contents = await file.read()  # Use await to properly read the file
+    image = Image.open(BytesIO(contents))
+    return image
 
 @app.get("/find")
-def get_image(index_of_image: int):
+async def get_image(index_of_image: int):
+
     prediction = predict_cellphone(list_of_image[index_of_image])
-    temporary_variable = index_of_image
-    return {"prediction": prediction,
-            "strning":list_of_image[index_of_image]}
+    return {"prediction": prediction}
 
+@app.post("/detect_cellphone/")
+async def detect_cellphone_endpoint(file: UploadFile = File(...)):
+    image = await read_imagefile(file)  # Use await since read_imagefile is an async function
 
-
-@app.post("/predict/")
-def predict(file: UploadFile = File(...)):
-    # with open(file.phone_image, "wb") as buffer:
-    #     buffer.write(file.read())
-
-    list_of_image.append(file)
-    prediction = predict_cellphone(file)
-    return {"prediction": prediction ,
-            "thi": file}
+    list_of_image.append(image)
+    print("appended into the array")
+    prediction = predict_cellphone(image)
+    return {"prediction": prediction}
--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 # app/utils.py
-from torchvision.io import read_image
+# from torchvision.io import read_image
 from torchvision.models import resnet50, ResNet50_Weights
 
 # img = read_image("image.jpg")
@@ -14,10 +14,16 @@ preprocess = weights.transforms()
 
 
 def predict_cellphone(img):
-
-    image = read_image(img)
-    batch = preprocess(image).unsqueeze(0)
+    # image = read_image(img)
+    
+    batch = preprocess(img).unsqueeze(0)
     prediction = model(batch).squeeze(0).softmax(0)
     class_id = prediction.argmax().item()
     category_name = weights.meta["categories"][class_id]
     return category_name
+    # return {'aa':'avar'}
+
+
+
+# print(predict_cellphone("image.jpg"))
+
