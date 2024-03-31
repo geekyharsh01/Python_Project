c9028434bd6134f087011356ef2111df7b5fe35b
Harsh Kumar <95271790+geekyharsh01@users.noreply.github.com>
2024-03-31 08:07:58 +0530

    Update main.py and models.py , Added python logs

--- api/main.py
+++ api/main.py
@@ -2,9 +2,15 @@
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 from io import BytesIO
+import logging
 
 #importing predict_cellphone function from the models.py here 
 from models import predict_cellphone
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 # Initialize FastAPI instance
 app = FastAPI()
@@ -22,17 +28,22 @@ async def read_imagefile(file) -> Image.Image:
     Returns:
     - Image.Image: The PIL Image object representing the uploaded image.
     """
-    # Read the contents of the uploaded file asynchronously
-    contents = await file.read()  
+    try:
+        # Read the contents of the uploaded file asynchronously
+        contents = await file.read()  
 
-    # Open the image using PIL's Image module from the read contents
-    image = Image.open(BytesIO(contents)) 
+        # Open the image using PIL's Image module from the read contents
+        image = Image.open(BytesIO(contents)) 
 
-    # Return the PIL Image object representing the uploaded image
-    return image
+        # Return the PIL Image object representing the uploaded image
+        return image
+    except Exception as e:
+        # Log any exceptions that occur during image reading
+        logger.error(f"Error reading image file: {e}")
+        raise
 
 
-@app.get("/detected_results")
+@app.get("/detected_cellphone")
 async def get_prediction(index_of_image: int)->dict :
     """
     An endpoint to retrieve the prediction for an image at a specified index, using the list initiated above named list_of_prediction
@@ -43,16 +54,21 @@ async def get_prediction(index_of_image: int)->dict :
     Returns:
     - dict: A JSON response containing the prediction for the specified image index.
     """
-    # Calculate the length of the prediction list
-    length_of_list = len(list_of_prediction)
+    try:
+        # Calculate the length of the prediction list
+        length_of_list = len(list_of_prediction)
 
-    # Check if the index is within the range of available predictions
-    if(index_of_image<length_of_list):
-        # If the index is within range, return the prediction for the specified image
-        return {"prediction": list_of_prediction[index_of_image]}
-    
-     # If the index is out of range, return a message indicating that it is out of range.
-    return {"index": "out of range"}
+        # Check if the index is within the range of available predictions
+        if index_of_image < length_of_list:
+            # If the index is within range, return the prediction for the specified image
+            return {"prediction": list_of_prediction[index_of_image]}
+        # If the index is out of range, return a message indicating that it is out of range.
+        return {"index": "out of range"}
+    except Exception as e:
+        # Log any exceptions that occur during prediction retrieval
+        logger.error(f"Error retrieving prediction: {e}")
+        raise
+
 
 
 # Define a POST endpoint to detect cellphone in an uploaded image
@@ -69,15 +85,19 @@ async def detect_cellphone_endpoint(file: UploadFile = File(...)) ->dict:
     '''
 
 
-    # Read the uploaded image asynchronously using read_imagefile function
-    image = await read_imagefile(file)  
+    try:
+        # Read the uploaded image asynchronously using read_imagefile function
+        image = await read_imagefile(file)  
 
+        # Make a prediction for cellphone detection using predict_cellphone function
+        prediction = predict_cellphone(image)
 
-    # Make a prediction for cellphone detection using predict_cellphone function
-    prediction = predict_cellphone(image)
-
-    #appending the prediction of the input image to the list so that it can be retrieved when using get method.
-    list_of_prediction.append(prediction)
-    
-    # Return the prediction result as a JSON response
-    return {"prediction": prediction}
+        # Appending the prediction of the input image to the list
+        list_of_prediction.append(prediction)
+        
+        # Return the prediction result as a JSON response
+        return {"prediction": prediction}
+    except Exception as e:
+        # Log any exceptions that occur during cellphone detection
+        logger.error(f"Error Reading image file or detecting cellphone: {e}")
+        raise
--- api/models.py
+++ api/models.py
@@ -1,6 +1,12 @@
 # Importing Torchvision Library that will help in importing Resnet model which
 #which will be used to predict whether the input image is cellphone or not.
 from torchvision.models import resnet50, ResNet50_Weights
+import logging
+
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 # Initialize model with the best available weights
@@ -19,26 +25,26 @@ def predict_cellphone(img):
     This is the function which takes image as input and provides the category name "cellular telephone" as output.
     This function is going to be used by the detect_cellphone_endpoint in main.py file 
     '''
-    #Applying Preprocessing on the img and then adding batch dimension.
-    batch = preprocess(img).unsqueeze(0) 
 
+    try:
+        # Applying Preprocessing on the img and then adding batch dimension.
+        batch = preprocess(img).unsqueeze(0) 
 
-    #Predicting using the model created above and then squeezing the output given by the model and applying softmax to get probablities for each label.
-    prediction = model(batch).squeeze(0).softmax(0) 
+        # Predicting using the model created above and then squeezing the output given by the model and applying softmax to get probabilities for each label.
+        prediction = model(batch).squeeze(0).softmax(0) 
 
+        # Now getting the class_id correspond to the highest probability value, because the image we provide contains the cellular telephone so its id will be stored in the variable class_id
+        class_id = prediction.argmax().item()
 
-    #Now getting the class_id correspond to the highest probablity value, because the image we provide contains the cellular telephone so its id will be stored in the variable class_id
-    class_id = prediction.argmax().item()
+        # Now getting the category_name on the basis of class_id, weights here contains the information of weight of pretrained model also the categories used while training the model.
+        category_name = weights.meta["categories"][class_id]
 
-
-    #Now getting the category_name on the basis of class_id, weights here contains the information of wieght of pretrained model also the categories used while training the model.
-    category_name = weights.meta["categories"][class_id]
-
-
-    #Now returning the category name
-    return category_name
-
-
+        # Now returning the category name
+        return category_name
+    except Exception as e:
+        # Log any exceptions that occur during cellphone prediction
+        logger.error(f"Error predicting cellphone: {e} , Error in Model predict_cellphone")
+        raise
 
 
 
